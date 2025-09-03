b16b06c1172cf6cde03516f0ec97535bfd01780a
gamer zero <kayo.mmenezes@aluno.edu.es.gov.br>
2025-09-03 11:58:51 -0300

    Create index.php

---
+++ aula1-3trimesttre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Aula 1 PHP</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello Word";
+            echo "Kayo M.M. e Isabelly Binda";
+        ?>
+    </body>
+</html>
