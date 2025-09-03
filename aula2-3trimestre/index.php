67d5e95f0eae3b7519dca8c1e040b58ffe65235b
gamer zero <kayo.mmenezes@aluno.edu.es.gov.br>
2025-09-03 11:59:40 -0300

    Create index.php

---
+++ aula2-3trimestre/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Aula 2-PHP</title>
+    </head>
+    <body>
+        <?php
+            $nome1="Kayo Magnani Menezes";
+            $nome2="Isabelly binda";
+            echo "Meu nome é " .$nome1 ." E minha dupla é " .$nome2;
+        ?>
+    </body>
+</html>
