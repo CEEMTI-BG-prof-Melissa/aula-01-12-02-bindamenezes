0dd4353e8d5678e36371180bef9d7bc372bfda7a
gamer zero <kayo.mmenezes@aluno.edu.es.gov.br>
2025-09-09 11:56:21 -0300

    Create index.php

---
+++ Aula03-3°tri/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Aula 03 -PHP</title>
+    </head>
+    <body>
+        <?php
+            $nome1="Kayo Magnani Menezes";
+            $nome2="Isabelly Binda";
+
+            $n1=10;
+            $n2=2;
+            $soma= $n1 + $n2;
+            
+            
+            echo "Meu nome é " .$nome1 ." e minha dupla é " .$nome2;
+            echo "<br>Meu nome é $nome1 e minha dupla é $nome2";
+            echo "<br><br> A soma de $n1 + $n2 = $soma";
+        ?>
+    </body>
+</html>
