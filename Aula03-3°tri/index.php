aeff56022127c7d9cb6216c3bb68d56fe094cdbf
gamer zero <kayo.mmenezes@aluno.edu.es.gov.br>
2025-09-10 11:55:58 -0300

    Update index.php

--- Aula03-3°tri/index.php
+++ Aula03-3°tri/index.php
@@ -15,6 +15,10 @@
             echo "Meu nome é " .$nome1 ." e minha dupla é " .$nome2;
             echo "<br>Meu nome é $nome1 e minha dupla é $nome2";
             echo "<br><br> A soma de $n1 + $n2 = $soma";
+            echo "<br><br> A subtração de $n1 e $n2 é igual a: " .($n1 - $n2). ".";
+            echo "<br><br> A multiplicação de $n1 e $n2 é igual a: " .($n1 * $n2). ".";
+            echo "<br><br> A divisão de $n1 e $n2 é igual a: " .($n1 / $n2). ".";
+            echo "<br><br> O resto da divisão de $n1 e $n2 é igual a: " .($n1 % $n2). ".";
         ?>
     </body>
 </html>
